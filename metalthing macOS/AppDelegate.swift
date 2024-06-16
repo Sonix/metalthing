1c115e72a1a9b44a79a736040ada4cfe3b65a4f1
Johannes Loepelmann <johannes@loepelmann.de>
2024-06-16 11:57:00 +0200

    Initial Commit

---
+++ metalthing macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  metalthing macOS
+//
+//  Created by Johannes Loepelmann on 16.06.24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
